129084bd37f204e8b3913a3e14c31e6bc20b4cf2
Philip Brownlow <philip.brownlow777@gmail.com>
2023-07-23 18:29:08 +0100

    cleanup: add comments to dbmanager's types.go

--- internal/dbmanager/types.go
+++ internal/dbmanager/types.go
@@ -8,12 +8,23 @@ var (
 	dbExistsError = "database '%s' already exists"
 )
 
+// DBManager is the interface for any DB manager instances
 type DBManager interface {
+	// Returns the map of DB instance names to their respective DB instance
 	GetDBs() map[string]db.DB
+
+	// Creates a DB instance and adds it to the DB map
+	// Returns an error if the DB already exists
+	// Returns an error if the keyHeader is empty
+	// Returns an error if the list of headers is empty or the keyHeader is not in the list
 	CreateDB(name string, headers []db.HeaderI, keyHeader string) error
+
+	// Returns true if the DB exists in the DBManager's map of DBs
 	DBExists(name string) bool
 }
 
+// The implementation for DBManager holding the following fields:
+// DBs: the map containing the name of the DB mapped to the DB instance
 type DBManagerImpl struct {
 	DBs map[string]db.DB
 }
